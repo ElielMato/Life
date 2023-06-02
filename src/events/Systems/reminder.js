2d6a5a78178157ab34a2e855a35657a0fa36a389
ElielMato <86053896+ElielMato@users.noreply.github.com>
2023-06-02 00:51:28 -0300

    Reminder Update

---
+++ src/events/Systems/reminder.js
@@ -0,0 +1,37 @@
+const { EmbedBuilder } = require("discord.js");
+const Config = require('../../../config.json');
+const reminderSchema = require('../../models/reminder.schema.js')
+const Guild = require('../../models/guilds')
+
+module.exports = {
+    name: 'interactionCreate',
+    async execute(interaction, client) {
+
+        const locale = await Guild.findOne({ guildId: interaction.guild.id })
+        const language = locale.lang
+        setInterval(async () => {
+            const reminders = await reminderSchema.find();
+            if (!reminders) return;
+            else {
+                reminders.forEach(async reminder => {
+
+                    if (reminder.Time > Date.now()) return;
+
+                    const user = await client.users.cache.get(reminder.User);
+                    const embedReminder = new EmbedBuilder()
+                        .setTitle(client.languages.__({phrase: "reminder.title", locale: language}))
+                        .setDescription(reminder.Message)
+                        .setColor(Config.color.CELE)
+                        .setTimestamp()
+                    user?.send({ embeds: [embedReminder] }).catch(err => {return});
+
+                    await reminderSchema.deleteMany({
+                        User: user.id,
+                        Time: reminder.Time,
+                        Message: reminder.Message
+                    });
+                });
+            };
+        }, 1000 * 5)
+    }
+}
